066f42b0dce09b5c296dd0de288a6a54026b6e91
Vadim252525 <objectgm@gmail.com>
2025-10-03 22:56:42 +0300

    Version 1.2.0

---
+++ CreateLevels/src/main/java/ts/createlevels/Main.java
@@ -0,0 +1,32 @@
+package ts.createlevels;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import ts.createlevels.Forms.MainScene;
+
+import java.io.File;
+
+import static ts.createlevels.ProgramSettings.*;
+
+public class Main extends Application
+{
+    @Override
+    public void start(Stage stage)
+    {
+        if(!new File("save").exists()) createDirectory("save");
+
+        stage.setTitle("Создание 2D уровней - " + VERSION);
+        stage.setMaximized(true);
+        stage.setWidth(WIDTH);
+        stage.setHeight(HEIGHT);
+        Image icon = new Image("file:icon.png");
+        stage.getIcons().add(icon);
+        stage.show();
+        MainScene.open(stage);
+    }
+    public static void main(String[] args)
+    {
+        launch();
+    }
+}
---
+++ CreateLevels/src/main/java/ts/createlevels/ProgramSettings.java
@@ -0,0 +1,67 @@
+package ts.createlevels;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ProgramSettings
+{
+    public static final int WIDTH = 1920, HEIGHT = 1080;
+    public static final String VERSION = "1.2.0";
+    public static boolean isCanvasOpen = false, isWindowCloseRequestEvent = false;
+    public static void createDirectory(String path)
+    {
+        try
+        {
+            Files.createDirectory(Paths.get(path));
+        }
+        catch (IOException e)
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
+    public static BackgroundFill bgFill()
+    {
+        return new BackgroundFill(
+                new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
+                        new Stop(0, Color.web("#3a3a3a")),
+                        new Stop(0.25f, Color.web("#222222")),
+                        new Stop(1, Color.BLACK)),
+                CornerRadii.EMPTY,
+                Insets.EMPTY);
+    }
+    public static BackgroundFill bgSceneFill()
+    {
+        return new BackgroundFill(Color.web("#121212"), CornerRadii.EMPTY, Insets.EMPTY);
+    }
+    public static ImageView icon(String name)
+    {
+        ImageView iconFile = new ImageView("file:imageIcon/" + name + ".png");
+        iconFile.setFitHeight(24);
+        iconFile.setFitWidth(24);
+        return iconFile;
+    }
+    public static Label label(String text)
+    {
+        Label textL = new Label(text);
+        textL.setFont(Font.font("Arial", FontWeight.BOLD, 16f));
+        textL.setTextFill(Color.WHITE);
+        textL.setPadding(new Insets(7,0,0,0));
+        return textL;
+    }
+}
